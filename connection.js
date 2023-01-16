1006caf05b7d47777d4d158e1067533a803f0603
semnet1 <100323961+semnet1@users.noreply.github.com>
2023-01-16 19:04:02 -0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 // conexão com o server
-const socket = io("http://10.0.0.102:3000");
+const socket = io("https://Minesweeper.semnet1.repl.co");
 socket.on("connect", () => {
     console.log("You connected with id: " + socket.id);
 });
